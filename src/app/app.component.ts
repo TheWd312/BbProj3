3514609d959ad6c6b5dea3e8bf0adcb627e872de
zakaullah <pa638612@gmail.com>
2024-09-29 22:52:56 +0530

    msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardHeader} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BbProj3';
+  profiles = [ // Add dummy profiles
+    {
+      name: 'Rahul Roy',
+      about: 'About Winner 1. Lorem ipsum dolor sit amet.',
+      photo: '/assets/s1Winner.webp',
+      profileLink: 'https://www.instagram.com/officialrahulroy/?hl=en'
+    },
+    {
+      name: 'Ashutosh kaushik',
+      about: 'About Winner 2. Lorem ipsum dolor sit amet.',
+      photo: '/assets/s2winner.webp',
+      profileLink: 'https://www.instagram.com/iamashutoshkaushik/?hl=en'
+    },
+    {
+      name: 'Vindu Dara Singh',
+      about: 'About Winner 3. Lorem ipsum dolor sit amet.',
+      photo: '/assets/s3Winner.webp',
+      profileLink: 'https://www.instagram.com/vindusingh/?hl=en'
+    },
+    {
+      name: 'Shweta Tiwari',
+      about: 'About Winner 4. Lorem ipsum dolor sit amet.',
+      photo: '/assets/s4winner.webp',
+      profileLink: 'https://www.instagram.com/shweta.tiwari/?hl=en'
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
